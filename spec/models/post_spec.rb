90310541e45cdda80e8b1cd7f0b603d919a7c1ae
Nikki A <nikki.apostolakis@gmail.com>
2019-01-17 00:04:04 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-<<<<<<< HEAD
+#<<<<<<< HEAD
 
 describe Post do
   it 'can be created' do
@@ -7,8 +7,8 @@ describe Post do
     expect(post).to be_valid
   end
 end
-=======
+#=======
 
 #describe Post do
 #end
->>>>>>> 4283a3488e10f3687f184122a6ee7e91926f896d
+#>>>>>>> 4283a3488e10f3687f184122a6ee7e91926f896d
