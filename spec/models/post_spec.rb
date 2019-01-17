c1022d3e92d8d9297407ea2e6f07189a0d3ef57c
Nikki A <nikki.apostolakis@gmail.com>
2019-01-17 00:20:36 +0000

    Done.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
+#<<<<<<< HEAD
 
 describe Post do
+  it 'can be created' do
+    post = Post.create!(title: "My title", description: "The post description")
+    expect(post).to be_valid
+  end
+
+  it 'has a summary' do
+    post = Post.create!(title: "My title", description: "The post description")
+    expect(post.post_summary).to eq("My title - The post description")
+  end
 
 end
+#=======
+
+#describe Post do
+#end
+#>>>>>>> 4283a3488e10f3687f184122a6ee7e91926f896d
