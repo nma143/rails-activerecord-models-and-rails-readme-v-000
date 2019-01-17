587ead4e4f5414bd5ad075be806ab974e8dd2ec1
Nikki A <nikki.apostolakis@gmail.com>
2019-01-17 00:02:10 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
-<<<<<<< HEAD
+#<<<<<<< HEAD
 
 describe Post do
-
+  it 'can be created' do
+    post = Post.create!(title: "My title", description: "The post description")
+    expect(post).to be_valid
+  end
 end
-=======
+#=======
  
-describe Post do
- 
-end
->>>>>>> 4283a3488e10f3687f184122a6ee7e91926f896d
+#describe Post do
+#end
+#>>>>>>> 4283a3488e10f3687f184122a6ee7e91926f896d
